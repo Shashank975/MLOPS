7d52d1c360affa7ad29020532bc03f1797f58452
Shashank Chhoker <shashanksaad07@gmail.com>
2024-11-09 22:56:48 +0530

    Folder Structure Upadated!

---
+++ templates.py
@@ -0,0 +1,47 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/logger/logging.py",
+    "src/exceptions/exceptions.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+    "src/utils/__init__.py",
+    "src/utils/utils.py",
+    "test/unit/__init__.py",
+    "test/integration/__init__.py",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiments.ipynb"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    # Ensure the directory exists
+    if filedir:
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir} for file: {filename}")
+
+    # Create the file if it doesn't exist or is empty
+    if not filepath.exists() or filepath.stat().st_size == 0:
+        with open(filepath, 'w') as f:
+            pass
+        logging.info(f"Created or cleared file: {filepath}")
